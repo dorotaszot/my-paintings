1947d65d75af8bcee2d85c4b005c11c48630cc24
dorotaszot <62651157+dorotaszot@users.noreply.github.com>
2020-07-06 13:30:59 +0200

    about added

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
-// MDB Lightbox Init
-$(function () {
-  $("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
-  });
 console.log('ok');
+
+// $(window).scroll(function(){
+//   if ($(this).scrollTop() > 1000) {
+//       $('.to-top').css("display", "block");
+//   } else {
+//       $('.to-top').css("display", "none");
+//   }
+// });
+
+// $('.to-top').on('click', function () {
+//   const images = $('#to-top').position().top;
+
+//   $('html, body').animate(
+//     {
+//       scrollTop: images
+//     }, 800)
+// });
+
+
+
+
+
